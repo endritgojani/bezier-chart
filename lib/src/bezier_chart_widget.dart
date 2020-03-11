78ca259c09a468e7232df328030f55b1375e19b2
Endrit Gojani <>
2020-03-11 15:09:43 +0100

    no message

--- lib/src/bezier_chart_widget.dart
+++ lib/src/bezier_chart_widget.dart
@@ -158,10 +158,10 @@ class BezierChartState extends State<BezierChart>
   bool _displayIndicator = false;
 
   ///padding for leading and trailing of the chart
-  final double horizontalPadding = 50.0;
+  final double horizontalPadding = 0.0;
 
   ///spacing between each datapoint
-  double horizontalSpacing = 60.0;
+  double horizontalSpacing = 0.0;
 
   ///List of `DataPoint`s used to display all the values for the `X` axis
   List<DataPoint> _xAxisDataPoints = [];
